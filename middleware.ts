6e0654a3bb7ca76ecd586a570a00cd9a8f6271c2
xuantruongg003 <lexuantruong098@gmail.com>
2026-02-01 16:22:38 +0700

    feat(middleware): update matcher to exclude specific text files from routing

--- middleware.ts
+++ middleware.ts
@@ -96,5 +96,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\..*|robots.txt|sitemap.xml).*)"],
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\..*|robots.txt|sitemap.xml|[a-f0-9]{32}\\.txt).*)"],
 };
